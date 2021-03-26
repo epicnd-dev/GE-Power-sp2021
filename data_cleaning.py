adc7f625856d62dc0951576077110f099faee440
John Flaherty <jflaher4@nd.edu>
2021-03-25 20:10:40 -0400

    data clean 2

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,9 +2,8 @@
 from scipy import stats
 import pandas as pd
 import numpy as np
-
 df = pd.read_csv('Train/gt_2011.csv')
-
 z = np.abs(stats.zscore(df))
-i = np.where(z>7)
-print(i[0].shape)
+i = np.where(z>5)
+print(list(zip(i[0],i[1])))
+print("Number of outliers:",i[0].shape)
