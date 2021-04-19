711ef9bee106c56f1d68e23caa39aee42e66af57
tlincoln112 <tlincoln@nd.edu>
2021-04-19 19:45:01 -0400

    Changed linear model to incoperate all train data

--- Models/Linear_Regression/linear_reg.py
+++ Models/Linear_Regression/linear_reg.py
@@ -5,9 +5,13 @@ from numpy import loadtxt
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-df_train = pd.read_csv('Data/Train/gt_2011.csv', delimiter=',')
-df_test = pd.read_csv('Data/Train/gt_2012.csv', delimiter=',')
+# use 2013 for testing
+train_1 = pd.read_csv('Data/Train/gt_2011.csv', delimiter=',')
+train_2 = pd.read_csv('Data/Train/gt_2012.csv', delimiter=',')
+test_1 = pd.read_csv('Data/Test/gt_2013.csv', delimiter=',')
+frames = [train_1, train_2]
+df_train = pd.concat(frames)
+print(len(df_train))
 
 inputs = ['AT','AP','AH','AFDP','GTEP','TIT','TAT']
 outputs = ['TEY']
@@ -15,30 +19,28 @@ outputs = ['TEY']
 input_df = pd.DataFrame(df_train, columns=inputs)
 output_df = pd.DataFrame(df_train, columns=outputs)
 
+
 X = input_df.values.tolist()
 Y = output_df.values.tolist()
 
 
+test_in = pd.DataFrame(test_1, columns=inputs)
+test_out = pd.DataFrame(test_1, columns=outputs)
 
-testin_df = pd.DataFrame(df_test, columns=inputs)
-testout_df = pd.DataFrame(df_test, columns=outputs)
-
-x = testin_df.values.tolist()
-y = testout_df.values.tolist()
+x = test_in.values.tolist()
+y = test_out.values.tolist()
 
 reg = LinearRegression().fit(X, Y)
 
-print("The R^2 of the Model is: ", reg.score(X, Y))
+print("The R^2 of the Model is: ", reg.score(x, y))
 
-pred2012 = reg.predict(x)
+pred2013 = reg.predict(x)
 
-# np.savetxt("2012_Linear_Predictions.csv", pred2012, delimiter=" ")
+np.savetxt("Models/Linear_Regression/2013_Linear_Predictions.csv", pred2013, delimiter=" ")
 
-plt.plot(pred2012)
+#plt.plot(pred2012)
 
-plt.ylabel('TEY')
+#plt.ylabel('TEY')
 # plt.show()
 
 print("The coefficients of each variable in the model: ", reg.coef_)
-
-
